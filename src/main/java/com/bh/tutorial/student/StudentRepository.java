7ddd332bf8a6530fe3107890d0e3043419a5c5e0
brian <brianhwang29@gmail.com>
2021-06-27 16:27:16 +0900

    mapping sql

--- src/main/java/com/bh/tutorial/student/StudentRepository.java
+++ src/main/java/com/bh/tutorial/student/StudentRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-    //SELECT * FROM Studdent WHERE email = ?
+    //SELECT * FROM Student WHERE email = ?
     @Query("SELECT s FROM Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
 }
